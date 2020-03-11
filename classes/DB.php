0f876eb81bc8656fe04fefbae880bf32ceb6440d
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2020-03-12 03:47:19 +0600

    Database Querying

--- classes/DB.php
+++ classes/DB.php
@@ -24,6 +24,60 @@ class DB {
     return self::$_instance;
   }
 
+  private function query($sql, $params = array()) {
+    $this->_error = false; // Reset Error Message
+    if($this->_query = $this->_pdo->prepare($sql)) {
+      $x = 1;
+      if(count($params)) {
+        foreach ($params as $param) {
+          $this->_query->bindValue($x, $param);
+          $x++;
+        }
+      }
+      if($this->_query->execute()) {
+        $this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
+        $this->_count = $this->_query->rowCount();
+      } else {
+        $this->_error = true;
+      }
+    }
+    return $this;
+  }
+
+  public function action($action, $table, $where = array()) {
+     if(count($where) === 3) {
+       $operators = array('=', '>', '<', '>=', '<=');
+
+       $field     = $where[0];
+       $operator  = $where[1];
+       $value     = $where[2];
+
+       if (in_array($operator, $operators)) {
+         $sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";
+
+         if(!$this->query($sql, array($value))->error()) {
+           return $this->_results;
+         }
+       }
+     }
+  }
+
+  public function get($table, $where = array()) {
+    return $this->action('SELECT *', $table, $where);
+  }
+
+  public function delete($table, $where = array()) {
+    return $this->action('DELETE', $table, $where);
+  }
+
+  public function error() {
+    return $this->_error;
+  }
+
+  public function count() {
+    return $this->_count;
+  }
+
 
 
 }
--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 require_once 'core/init.php';
 
-DB::getInstance();
+$user = DB::getInstance();
+$users = $user->get("groups", array('id', '=', 9));
+
+if($user->count()) {
+  echo "ok";
+} else {
+  echo "No User";
+}
+
 ?>
